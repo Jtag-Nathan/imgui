6cc31afb8b4fd9e56db3909bce0302e635ae29fc
JTAG <nathanjames001@hotmail.co.uk>
2024-08-19 06:34:43 +0100

    Added arch to premake file.

--- premake5.lua
+++ premake5.lua
@@ -2,6 +2,7 @@ project "ImGui"
 	kind "StaticLib"
 	language "C++"
     staticruntime "on"
+	architecture "x86_64"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
